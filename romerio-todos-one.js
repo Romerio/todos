bafb0a34619a3bf19ab6a276dfddc8bf95a6a16e
Romerio <romerio.unix@gmail.com>
2015-09-18 01:10:44 -0300

    Em casa

--- romerio-todos-one.js
+++ romerio-todos-one.js
@@ -1,9 +1,32 @@
 Todos = new Mongo.Collection("todos");
 
 if(Meteor.isClient){
+
   Template.todos.helpers({
     "todo": function() {
       return Todos.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.todoItem.helpers({
+    "checked": function() {
+      var isCompleted = this.completed;
+
+      if(isCompleted){
+        return "checked";
+      } else {
+        return "";
+      }
+    }
+  });
+
+  Template.todosCount.helpers({
+    "totalTodos": function() {
+      return Todos.find().count();
+    },
+
+    "completedTodos": function() {
+      return Todos.find({completed: true}).count();
     }
   });
 
@@ -31,25 +54,43 @@ if(Meteor.isClient){
         Todos.remove({_id: documentId});
     },
 
+    'change [type="checkbox"]': function(event) {
+      var documentId = this._id;
+      var isCompleted = this.completed;
+
+      if (isCompleted){
+        Todos.update(documentId, {$set: {completed: false }});
+        console.log("Task marked as incomplete.");
+      } else {
+        Todos.update(documentId, {$set: {completed: true }});
+        console.log("Task marked as complete.");
+      }
+    },
+
     'keyup [name="todoItem"]': function(event) {
       var todoName = $(event.target).val();
       var documentId = this._id;
 
+      console.log(event.which);
+
       if(event.which == 13){
         Todos.update({_id: documentId}, {$set: {name: todoName} });
-      } else if(event.which == 27){
-
+        $(event.target).blur();
       }
-    },
 
+    }
+    /* Salva alterações no TAB
     'keypress [name="todoItem"]': function(event) {
       var todoName = $(event.target).val();
       var documentId = this._id;
 
+      console.log(event.which);
+
       if(event.which == 0){
-        Todos.update({_id: documentId}, {$set: {name: todoName} });
+        //Todos.update({_id: documentId}, {$set: {name: todoName} });
+        $(event.target).blur();
       }
-    }
+    }*/
   });
 
 }
